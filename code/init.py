a2b18107d4ca17b2605212149153eec760896c4e
Sandy MacDonald <59518103+Sandalots@users.noreply.github.com>
2022-09-19 01:20:18 +0100

    added init boilerplate

--- code/init.py
+++ code/init.py
@@ -1,5 +1,18 @@
-# Python boilerplate for the code directory
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
 
-if __name__ == 'main':
-    print('Hello, world!')
-    
+__author__ = "Your Name"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    """ Main entry point of the app """
+    print("hello world")
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
